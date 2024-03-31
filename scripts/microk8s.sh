8d930c0a63e5692662e31b35cda9c79fe494bc39
statsmind <jameshu@live.com>
2024-03-31 16:16:58 +0800

    init microk8s setup script

---
+++ scripts/microk8s.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+proxy_host=${1:-"registry.statsmind.com"}
+echo "proxy_host: ${proxy_host}"
+
+microk8s_certs_path=/var/snap/microk8s/current/args/certs.d
+
+registries=(docker.io gcr.io ghci.io k8s.gcr.io nvcr.io quay.io registry.k8s.io)
+
+for reg in ${registries[@]}; do
+  echo "setup ${reg} hosts file"
+  mkdir -p ${microk8s_certs_path}/${reg} && cat > ${microk8s_certs_path}/${reg}/hosts.toml <<EOF
+[host."${proxy_host}"]
+  capabilities = ["pull", "resolve"]
+  skip_verify = true
+EOF
+done
+
+if [ -f ${microk8s_certs_path}/_default/hosts.toml ]; then
+  echo "remove _default hosts file"
+  mv ${microk8s_certs_path}/_default /tmp
+fi
+
+if [ -f ${microk8s_certs_path}/localhost:32000/hosts.toml ]; then
+  echo "remove localhost:32000 hosts file"
+  mv ${microk8s_certs_path}/localhost:32000 /tmp
+fi

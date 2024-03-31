448976ea682bf2092e3c953ee213c88c66cc8b6c
statsmind <jameshu@live.com>
2024-03-31 16:24:36 +0800

    init microk8s setup script

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # setup registry proxy cache for microk8s
+curl "https://raw.githubusercontent.com/statsmind/registry-proxy/main/scripts/microk8s.sh" | bash -c "123"
--- scripts/microk8s.sh
+++ scripts/microk8s.sh
@@ -9,7 +9,7 @@ registries=(docker.io gcr.io ghci.io k8s.gcr.io nvcr.io quay.io registry.k8s.io)
 
 for reg in ${registries[@]}; do
   echo "setup ${reg} hosts file"
-  mkdir -p ${microk8s_certs_path}/${reg} && cat > ${microk8s_certs_path}/${reg}/hosts.toml <<EOF
+  sudo mkdir -p ${microk8s_certs_path}/${reg} && sudo cat > ${microk8s_certs_path}/${reg}/hosts.toml <<EOF
 [host."${proxy_host}"]
   capabilities = ["pull", "resolve"]
   skip_verify = true
@@ -18,10 +18,10 @@ done
 
 if [ -f ${microk8s_certs_path}/_default/hosts.toml ]; then
   echo "remove _default hosts file"
-  mv ${microk8s_certs_path}/_default /tmp
+  sudo mv ${microk8s_certs_path}/_default /tmp
 fi
 
 if [ -f ${microk8s_certs_path}/localhost:32000/hosts.toml ]; then
   echo "remove localhost:32000 hosts file"
-  mv ${microk8s_certs_path}/localhost:32000 /tmp
+  sudo mv ${microk8s_certs_path}/localhost:32000 /tmp
 fi
